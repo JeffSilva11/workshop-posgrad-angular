729a26fdf7ce7dab107e2593a1fa5ad644d043c7
Jefferson Silva <fj_adm@hotmail.com>
2024-10-05 23:40:21 -0300

    COMMIT: UN03-02 - Criado app.component.html e add angular/material

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,17 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <div style="text-align:center" class="content">
-      <h1>Welcome to {{title}}!</h1>
-      <span style="display: block">A aplicação {{title}} está rodando com sucesso!</span>
-      <img width="300" alt="Angular Logo" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZzHw1UlzFSWnKyJC0NZS413qWiw9bPCcXhk43DpIJRAYvaz1B9Pc3QWEo1Yvu"/>
-    </div>
-
-    <router-outlet />
-  `,
-  styles: [],  
+  templateUrl: 'app.component.html'  
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Frontend';
 }
